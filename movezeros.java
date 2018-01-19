f0955366a369a47cf40dca5d472ef4eb5e8e9243
mahima95 <mahimagupta1195@gmail.com>
2018-01-19 11:40:57 -0700

    Create movezeros.java

---
+++ movezeros.java
@@ -0,0 +1,20 @@
+class Solution
+{
+public void moveZeroes(int[] nums) {
+	int i = 0, j = 0;
+	while (j < nums.length) {
+		if (nums[j] != 0) {
+			int temp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = temp;
+            i++;
+		}
+		j++;
+	}
+}
+
+public void swap(int i, int j, int[] nums) {
+	
+}
+        
+    }
