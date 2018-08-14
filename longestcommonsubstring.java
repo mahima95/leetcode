af53eb17c17bffe50d2336c5ea1c57fc9e7804aa
Mahima Gupta <mahimagupta1195@gmail.com>
2018-08-14 05:54:36 -0700

    https://www.programcreek.com/2015/04/longest-common-substring-java/

--- longestcommonsubstring.java
+++ longestcommonsubstring.java
@@ -8,11 +8,11 @@ public static int getLongestCommonSubstring(String a, String b){
  
 	for(int i=0; i<m; i++){
 		for(int j=0; j<n; j++){
-			if(a.charAt(i) == b.charAt(j)){
+			if(a.charAt(i) == b.charAt(j)){ //check if same
 				if(i==0 || j==0){
 					dp[i][j]=1;
 				}else{
-					dp[i][j] = dp[i-1][j-1]+1;
+					dp[i][j] = dp[i-1][j-1]+1; //Checking the number of one previous block
 				}
  
 				if(max < dp[i][j])
@@ -24,3 +24,6 @@ public static int getLongestCommonSubstring(String a, String b){
  
 	return max;
 }
+
+
+//https://www.programcreek.com/2015/04/longest-common-substring-java/
